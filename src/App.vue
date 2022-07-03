35145ff330f7f64e27a2399d22c31651fbffa9a5
Thuong Nguyen <bimax270702@gmail.com>
2022-07-03 14:58:22 +0700

    ex vuejs-typescript

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="app">
+    <!-- <p>{{name}} - {{age}}</p>
+    <button @click="changeName('truc')">Change Name</button>
+    <button @click="changeAge('19')">Change Age</button> -->
+
+    <!-- <p v-for="job in jobs" :key="job.id">{{job.location}}</p> -->
+
+    <job-list :jobs="jobs"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, ref, toRef, toRefs } from 'vue';
+import Job from './types/jobs';
+import JobList from './components/JobList.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    JobList
+  },
+  setup(){
+// const state = reactive({
+//   name: 'Thuong',
+//   age: 20 as number | string
+// })
+// return {...toRefs(state)}
+//=======================
+// const name = ref('Thuong')
+// const age = ref<number | string>(20)
+// return {name, age}
+//=======================
+const jobs = ref<Job[]>([
+  {title: 'title1', location: 'thua thien hue1', salary: 50000, id: 1},
+  {title: 'title2', location: 'thua thien hue2', salary: 60000, id: 2},
+  {title: 'title3', location: 'thua thien hue3', salary: 70000, id: 3},
+  {title: 'title4', location: 'thua thien hue4', salary: 80000, id: 4},
+  {title: 'title5', location: 'thua thien hue5', salary: 90000, id: 5},
+  {title: 'title6', location: 'thua thien hue6', salary: 10000, id: 6},
+  {title: 'title7', location: 'thua thien hue7', salary: 30000, id: 7},
+])
+
+return {jobs}
+  },
+  methods:{
+    // changeName(name: string){
+    //   this.name = name
+    // },
+    // changeAge(age: number | string){
+    //   this.age = age
+    // }
+  }
+});
+</script>
+
+<style>
+</style>
---
+++ src/components/JobList.vue
@@ -0,0 +1,48 @@
+<template>
+    <ul>
+        <li v-for="job in jobs" :key="job.id">
+        <h2>{{job.title}} in {{job.location}}</h2>
+        <div class="salary">
+            <p>{{job.salary}} $</p>
+        </div>
+        <div class="description">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt officiis quidem facere deserunt! Repellendus, voluptates officia eum, sequi maiores, earum nemo ipsa asperiores unde id soluta mollitia a totam doloribus.</p>
+        </div>
+        </li>
+    </ul>
+</template>
+
+<script lang="ts">
+import Job from '@/types/jobs'
+import { defineComponent, PropType } from 'vue'
+
+export default defineComponent({
+    name: 'job-list',
+    props:{
+        jobs:{
+            type: Array as PropType<Job[]>,
+            required: true
+        }
+    }
+})
+</script>
+
+<style scoped>
+    .salary{
+        color: blue;
+        font-weight: bold;
+        font-size: 20px;
+    }
+    ul{
+        list-style: none;
+    }
+    .description{
+        font-size: 17px;
+    }
+    ul li{
+        border: 1px solid black;
+        margin: 5px;
+        padding: 5px 30px;
+         background-color: rgb(237, 223, 207);
+    }
+</style>
